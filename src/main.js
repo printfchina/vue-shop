c3cb019087b1f69c25badc05ac7c194d5f29e4e6
printfchina <1074053441@qq.com>
2020-11-23 15:52:06 +0800

    完成登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+// 导入按钮组件，表单组件，输入框组件，弹框组件
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message
