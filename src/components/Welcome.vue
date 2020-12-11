3efc8a714dc935abb8a0fcddae9cee285e0a4724
printfchina <1074053441@qq.com>
2020-12-12 01:20:20 +0800

    完成用户列表功能的开发

---
+++ src/components/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+      <h3>11111</h3>
+  </div>
+</template>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,33 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  Row,
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination,
+  Dialog,
+  MessageBox
+} from 'element-ui'
 // 导入按钮组件，表单组件，输入框组件，弹框组件
+// 注册
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -9,4 +36,20 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
